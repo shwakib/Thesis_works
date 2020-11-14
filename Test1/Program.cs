d95422ce3469dde65b56044d3edb1d60f807b53c
Sifat Hasan Wakib <sifat6460@gmail.com>
2020-11-14 17:56:05 +0600

    final touch

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test_Demo
+namespace Test1
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            
+            double addLoad = -1.0;
             int[] Nodes = { 50, 50, 50, 50, 100, 50, 50, 50, 50, 100 };
             double[] Loads = { 0.6, 0.8, 0.9, 0.95, 1.5, 0.6, 0.8, 1.30, 0.6, 0.78 };
             int Threshold = 1;
@@ -19,11 +20,11 @@ namespace Test_Demo
             string SecondarylinkMessage = "Offloading To Secondary link.";
             string PrimarylinkMessage = "Sending Through Primary Link.";
             string SentMessage = "Sent To the Core Network.";
-            double addLoad = -1.0;
+
             Console.WriteLine("Calculating LOAD");
-             bool checkLoad(double Load)
-             {
-                if(Load>Threshold)
+            bool checkLoad(double Load)
+            {
+                if (Load > Threshold)
                 {
                     Console.Write("Load is More.");
                     return true;
@@ -33,12 +34,12 @@ namespace Test_Demo
                     Console.WriteLine("Load is Less.");
                     return false;
                 }
-             }
+            }
 
-             bool checkCoreNode(int LoadNode)
-             {
-                int position = LoadNode +1;
-                if(Nodes[position]==100)
+            bool checkCoreNode(int LoadNode)
+            {
+                int position = LoadNode + 1;
+                if (Nodes[position] == 100)
                 {
                     //Console.WriteLine("ETa core na");
                     return true;
@@ -47,7 +48,7 @@ namespace Test_Demo
                 {
                     return false;
                 }
-             }
+            }
 
             double AddLoad(int LoadIndex)
             {
@@ -59,42 +60,53 @@ namespace Test_Demo
                 return TotalLoad;
             }
 
-            for (int i = 0; i < Loads.Length; i++)
+
+            bool CoreStatus;
+            bool loadresult;
+            for (int i = 0; i < Loads.Length ; i++)
             {
-                bool loadresult;//checkLoad(Loads[i]);
-                bool CoreStatus = checkCoreNode(i);
+                Console.WriteLine(addLoad);
                 if(addLoad<0)
                 {
-                    loadresult = Loads[i] > Threshold ? true : false;
-                    
-                }
-                else
-                {
                     loadresult = addLoad > Threshold ? true : false;
-                }
-                if (!loadresult)
-                {
-                    if(CoreStatus)
+                    //loadresult = Loads[i] > Threshold ? true : false;
+                    if (!loadresult)
                     {
-                        Console.Write(Notoffloading);
-                        Console.WriteLine(SentMessage);
-                        break;
+                        CoreStatus= checkCoreNode(i);
+                        if (CoreStatus)
+                        {
+                            Console.Write(Notoffloading);
+                            Console.WriteLine(SentMessage);
+                            break;
+                        }
+                        else
+                        {
+                            addLoad = AddLoad(i);
+                            Console.WriteLine("Eita Core na Bhai");
+                            Console.WriteLine(addLoad);
+                        }
+
                     }
                     else
                     {
-                        addLoad = AddLoad(i);
-                        Console.WriteLine("Eita Core na Bhai");
-                        Console.WriteLine(addLoad);  
+                        Console.WriteLine("Ki korbo ekhon?");
                     }
-                    
                 }
                 else
                 {
+                    loadresult = checkLoad(Loads[i]);
                     
+                    //Console.WriteLine("Load Is Less");
+                   // break;
                 }
+                
 
+                
+                
                 //break;
             }
+
         }
+        
     }
 }
---
+++ Test2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool CoreStatus;
+            bool loadresult;
+            double addLoad = -1.0;
+            int[] Nodes = { 50, 50, 50, 50, 100, 50, 50, 50, 50, 100 };
+            double[] Loads = { 0.6, 0.8, 0.9, 0.95, 1.5, 0.6, 0.8, 1.30, 0.6, 0.78 };
+            int Threshold = 1;
+            string OffloadMessage = "Needs to be offloaded.";
+            string Notoffloading = "No need of offloading.";
+            string SecondarylinkMessage = "Offloading To Secondary link.";
+            string PrimarylinkMessage = "Sending Through Primary Link.";
+            string SentMessage = "Sent To the Core Network.";
+            Console.WriteLine("Calculating LOAD");
+
+            bool checkLoad(double Load)
+            {
+                if (Load > Threshold)
+                {
+                    Console.Write("Load is More.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Load is Less.");
+                    return false;
+                }
+            }
+
+            bool checkCoreNode(int LoadNode)
+            {
+                int position = LoadNode + 1;
+                if (Nodes[position] == 100)
+                {
+                    //Console.WriteLine("ETa core na");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            double AddLoad(int LoadIndex,double previousLoad)
+            {
+                double currentLoad = previousLoad;
+                double nextHopLoad = Loads[LoadIndex + 1];
+                double TotalLoad = currentLoad + nextHopLoad;
+                //Console.WriteLine(TotalLoad);
+                //int TotalLoad;
+                return TotalLoad;
+            }
+
+            for(int i=0;i<Loads.Length;i++)
+            {
+                Console.WriteLine(Loads[i]);
+                if(addLoad<0)
+                {
+                    Console.WriteLine(Loads[i]);
+                    loadresult=Loads[i]>Threshold ? true : false;
+                    Console.WriteLine(loadresult);
+                    Console.WriteLine("Load is less");
+                    if (!loadresult)
+                    {
+                        CoreStatus = checkCoreNode(i);
+                        if(!CoreStatus)
+                        {
+                            Console.WriteLine("Next Hop is not Core");
+                            addLoad = AddLoad(i,Loads[i]);
+                            Console.WriteLine(addLoad);
+                        }
+                        else
+                        {
+                            Console.WriteLine(Notoffloading);
+                            Console.WriteLine(SentMessage);
+                        }
+                    }
+                    //Load Beshi,Ds and ogula ber korte hobe.
+                    else
+                    {
+                        double Ds = Loads[i] - Threshold;
+                        double Dl = Loads[i] - Ds;
+                        Console.WriteLine(Ds);
+                        Console.WriteLine(Dl);
+                        Console.WriteLine(SecondarylinkMessage);
+                        Console.WriteLine(SentMessage);
+                        Console.WriteLine(i);
+                        CoreStatus = checkCoreNode(i);
+                        Console.WriteLine(CoreStatus);
+                        if (!CoreStatus)
+                        {
+                            Console.WriteLine("Next Hop is not Core");
+                            addLoad = AddLoad(i, Loads[i]);
+                            Console.WriteLine(addLoad);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Next Hop Is Core.");
+                            Console.WriteLine(Notoffloading);
+                            Console.WriteLine(SentMessage);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Done");
+                    loadresult = addLoad > Threshold ? true : false;
+                    Console.WriteLine(loadresult);
+                    double Ds = addLoad - Threshold;
+                    double Dl = addLoad - Ds;
+                    Console.WriteLine(Ds);
+                    Console.WriteLine(Dl);
+                    Console.WriteLine(SecondarylinkMessage);
+                    Console.WriteLine(SentMessage);
+                    Console.WriteLine(i);
+                    CoreStatus = checkCoreNode(i);
+                    Console.WriteLine(CoreStatus);
+                    if (!CoreStatus)
+                    {
+                        Console.WriteLine("Next Hop is not Core");
+                        addLoad = AddLoad(i,Dl);
+                        Console.WriteLine(addLoad);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Next Hop Is Core.");
+                        Console.WriteLine(Notoffloading);
+                        Console.WriteLine(SentMessage);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
